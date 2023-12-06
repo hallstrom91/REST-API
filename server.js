411ee47579fd462b6051cc9e94203ba505df58c4
Simon Hällström <143492796+hallstrom91@users.noreply.github.com>
2023-12-06 12:35:57 +0100

    REST-API update with PUT and DELETE

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use(
 
 app.listen(process.env.PORT, () => {});
 
-//Log every incoming session
+//Log every incoming session and user-ip in CMD
 app.use((request, response, next) => {
   console.log(
     `[${new Date().toLocaleString()}] [USER-IP ${request.ip}] ${
@@ -113,8 +113,8 @@ app.get("/", (request, response) => {
 GET
 ============================
 */
-
-app.get("/register", function (request, response) {
+// get all users or singel-user with command /user?id=number or /users for all
+app.get("/users/:id?", function (request, response) {
   let sqlDB = "SELECT * FROM register";
   let parameters = [];
 
@@ -132,6 +132,7 @@ app.get("/register", function (request, response) {
   });
 });
 
+// get all comments or specific comment thru id /comments?id=number or /comments for all
 app.get("/comments", function (request, response) {
   let sqlDB = "SELECT * FROM comments";
   let parameters = [];
@@ -156,9 +157,10 @@ POST
 ============================
 */
 
-app.post("/register", function (request, response) {
+// POST new user to DB thru insomnia and json-format
+app.post("/addUser", function (request, response) {
   const { name, username, password, email } = request.body;
-
+  // confirm that no input value is empty
   if (!name && !username && !password && !email) {
     return response.status(400).send("Alla fält måste fyllas i!");
   } else if (!name) {
@@ -185,14 +187,150 @@ app.post("/register", function (request, response) {
   }
 });
 
+// Post new comment to DB thru insomnia and json-format
+app.post("/addComment", function (request, response) {
+  const { name, message } = request.body;
+  //confirm no empty values
+  if (!name && !message) {
+    return response
+      .status(400)
+      .send("Alla fält måste fyllas i för att göra ett inlägg!");
+  } else if (!name) {
+    return response.status(400).send("Fyll i namn!");
+  } else if (!message) {
+    return response.status(400).send("Fyll i ett meddelande");
+  } else {
+    const sqlDB = "INSERT INTO comments (name, message) VALUES (?, ?)";
+    const values = [name, message];
+
+    connectDB.query(sqlDB, values, function (err, result, fields) {
+      if (err) {
+        console.log("Error with query", err);
+        response.send(500).send("Server Error");
+      } else {
+        response.send("Ett nytt gästboks-inlägg är skapat!");
+      }
+    });
+  }
+});
+
 /*
 ============================
 PUT
 ============================
 */
 
+// change value on specific user thru /editUser/number
+app.put("/editUser/:id", function (request, response) {
+  if (!request.params && !request.params.id) {
+    return response.status(400).send("Ogiltig förfrågan.");
+  }
+
+  const userID = request.params.id;
+  const { name, username, password, email } = request.body;
+
+  if (!name && !username && !password && !email) {
+    return response
+      .status(400)
+      .send("Alla fält måste fyllas i för att ändras!");
+  } else if (!name) {
+    return response.status(400).send("Namn måste fyllas i för att ändras!");
+  } else if (!username) {
+    return response
+      .status(400)
+      .send("Användarnamn måste fyllas i för att ändras!!");
+  } else if (!password) {
+    return response
+      .status(400)
+      .send("Lösenord måste fyllas i för att ändras!!");
+  } else if (!email) {
+    return response.status(400).send("Email måste fyllas i för att ändras!!");
+  } else {
+  }
+  const sqlDB =
+    "UPDATE register SET name = ?, username = ?, password = ?, email = ? WHERE id = ? ";
+  const values = [name, username, password, email, userID];
+
+  connectDB.query(sqlDB, values, function (err, result, fields) {
+    if (err) {
+      console.log("Error with query", err);
+      response.status(500).send("Server Error");
+    } else {
+      response.send("Användaruppgifterna har uppdaterats!");
+    }
+  });
+});
+
+// change value on specific guestbook-comment thru /editComment/number
+app.put("/editComment/:id", function (request, response) {
+  if (!request.params && !request.params.id) {
+    return response.status(400).send("Ogiltig förfrågan");
+  }
+  const commentID = request.params.id;
+  const { name, message } = request.body;
+
+  if (!name && !message) {
+    return response.status(400).send("Fyll i all fält!");
+  } else if (!name) {
+    return response.status(400).send("Fyll i namn!");
+  } else if (!message) {
+    return response.status(400).send("Fyll i meddelande!");
+  } else {
+    const sqlDB = "UPDATE comments SET name = ?, message = ? WHERE id = ? ";
+    const values = [name, message, commentID];
+
+    connectDB.query(sqlDB, values, function (err, result, fields) {
+      if (err) {
+        console.log("Error with query", err);
+        response.status(500).send("Server Error");
+      } else {
+        response.send("Meddelande har uppdaterats!");
+      }
+    });
+  }
+});
+
 /*
 ============================
 DELETE
 ============================
 */
+//Delete users thru /deleteUser/ID
+app.delete("/deleteUser/:id", function (request, response) {
+  const userID = request.params.id;
+
+  if (!userID) {
+    return response.status(400).send("Ogiltig förfrågan!");
+  }
+  const sqlDB = "DELETE FROM register WHERE id = ? ";
+  const value = [userID];
+
+  connectDB.query(sqlDB, value, function (err, result, fields) {
+    if (err) {
+      console.error("Error with query", err);
+      response.status(500).send("Server Error");
+    } else {
+      response.send("Användare är borttagen ur databasen.");
+    }
+  });
+});
+
+//Delete comment thru /deleteComment/ID
+app.delete("/deleteComment/:id", function (request, response) {
+  const commentID = request.params.id;
+
+  if (!commentID) {
+    return response.status(400).send("Ogiltig förfrågan!");
+  }
+  const sqlDB = "DELETE FROM comments WHERE id = ? ";
+  const value = [commentID];
+
+  connectDB.query(sqlDB, value, function (err, result, fields) {
+    if (err) {
+      console.error("Error with query", err);
+      response.status(500).send("Server Error");
+    } else {
+      response.send("Gästboksinlägg är borttaget ur databasen.");
+    }
+  });
+});
